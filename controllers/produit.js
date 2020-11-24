03ea997bc5ab0744d5eb93366bc5c4ffa06cd659
MarkoWilliam <yowanwillim@gmail.com>
2020-11-24 09:40:49 +0300

    modification

--- controllers/produit.js
+++ controllers/produit.js
@@ -8,12 +8,35 @@ const db = mysql.createConnection({
     database: process.env.DATABASE,
 });
 
-
+//-------------List produit-----------------
 exports.produitList = (req, res) => {
     try {
         db.query('SELECT * FROM produit', async(error, results) => {
             res.json(results);
         })
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+exports.produitID = (req, res) => {
+    // const {id, nom, couleur, stock_dispo, statue, prix, prix_solde, etat, image } = req.body;
+    try {
+        // const id = req.body;
+        const { id } = req.params;
+        console.log(id);
+        const { nom, couleur, stock_dispo, statue, prix, prix_solde, etat, image } = req.body;
+        console.log("Id du produit", req.body);
+        db.query('UPDATE produit SET nom=?, couleur=?, stock_dispo=?, statue=?, prix=?, prix_solde=?, etat=?, image=?  WHERE id= ?', [nom, couleur, stock_dispo, statue, prix, prix_solde, etat, image, id], async(error, results) => {
+                if (error) {
+                    console.log(error)
+                } else {
+                    res.json(results);
+                    console.log('ok');
+                }
+
+            })
+            // [id, nom, couleur, stock_dispo, statue, prix, prix_solde, etat, image]
     } catch (err) {
         console.log(err);
     }
--- routes/produit.js
+++ routes/produit.js
@@ -2,6 +2,11 @@ const express = require('express');
 const produitController = require('../controllers/produit');
 const router = express.Router();
 
+//-------List---------
 router.get('/produitList', produitController.produitList);
+
+//-------Upade liste--------
+router.patch('/:id', produitController.produitID);
+
 
 module.exports = router;
